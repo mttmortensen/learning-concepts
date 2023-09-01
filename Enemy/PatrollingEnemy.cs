10f712e9c5553eece079d55769478df70e3ed5a6
Matt Mortensen <mtt.mortensen527@gmail.com>
2023-09-01 14:09:50 -0600

    fixed patrolling bug

--- Enemy/PatrollingEnemy.cs
+++ Enemy/PatrollingEnemy.cs
@@ -5,15 +5,13 @@ using UnityEngine;
 public class PatrollingEnemy : MonoBehaviour
 {
     public float speed = 2f;
-    public float direction = 1; // 1 = right, -1 = left 
+    private int direction = 1; // 1 for right, -1 for left
     public float rayLength = 0.5f; // Length of the raycast
-    public float waitTime = 1f; // Time to wait at the edge 
+    public float waitTime = 1f; // Time to wait at the edge
 
-    public float offset = 0.5f; // Distance between raycast and enemy
+    private bool isWaiting = false; // To check if the enemy is in waiting state
 
-    private bool isWaiting = false; // check for waiting state
-
-    [SerializeField] private Animator anim;
+    private Animator anim;
 
     private void Start()
     {
@@ -22,31 +20,30 @@ public class PatrollingEnemy : MonoBehaviour
 
     private void Update()
     {
-        if(!isWaiting)
+        if (!isWaiting)
         {
-            //Move enemy
+            // Move enemy
             transform.Translate(Vector2.right * direction * speed * Time.deltaTime);
 
-            //Set the walking animation 
+            // Set the walking animation
             anim.SetBool("IsWalking", true);
             anim.SetBool("IsIdle", false);
 
-            //Edge Dectection via rayCast creation
-            Vector2 rayStart = (direction == 1) ? transform.position + Vector3.right * offset : transform.position + Vector3.left * offset;
+            // Edge Detection via rayCast creation
+            Vector2 rayStart = (direction == 1) ? transform.position + Vector3.right : transform.position + Vector3.left;
             RaycastHit2D hit = Physics2D.Raycast(rayStart, Vector2.down, rayLength);
-
 
             if (hit.collider == null)
                 StartCoroutine(WaitAtEdge());
-        
         }
         else
         {
-            // Set the idle transition
+            // Set the idle animation
             anim.SetBool("IsWalking", false);
             anim.SetBool("IsIdle", true);
         }
     }
+
 
     IEnumerator WaitAtEdge()
     {
@@ -59,12 +56,11 @@ public class PatrollingEnemy : MonoBehaviour
         isWaiting = false;
     }
 
+
     void FlipSprite()
     {
         Vector3 localScale = transform.localScale;
         localScale.x *= -1;
         transform.localScale = localScale;
     }
-
 }
-
