0c49b26e20b576f917e07314ed29eda9caa6154a
Matt Mortensen <mtt.mortensen527@gmail.com>
2023-09-02 00:30:56 -0600

    added damage to plyr from atk

--- Enemy/EnemyAttack.cs
+++ Enemy/EnemyAttack.cs
@@ -4,23 +4,53 @@ using UnityEngine;
 
 public class EnemyAttack : MonoBehaviour
 {
+    private bool isPlayerInRange = false;
+
     [SerializeField] private float attackRange = 2f;
     [SerializeField] private LayerMask playerLayer;
     [SerializeField] private Animator anim;
+    [SerializeField] private float checkRate = 0.2f; // Check every 0.2 seconds
+    [SerializeField] private int attackDamage = 1;
 
-    // Update is called once per frame
+    private float nextCheck;
+
     void Update()
     {
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.right * transform.localScale.x, attackRange, playerLayer);
-
-        if (hit.collider != null && hit.collider.CompareTag("Player"))
+        if (Time.time > nextCheck)
         {
-            Attack();
+            nextCheck = Time.time + checkRate;
+            CheckForPlayer();
         }
     }
 
-    void Attack()
+    void CheckForPlayer()
     {
-        anim.SetTrigger("Attack");
+        Vector2 direction = Vector2.right * transform.localScale.x;
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, direction, attackRange, playerLayer);
+
+        if (hit.collider != null && hit.collider.CompareTag("Player"))
+        {
+            isPlayerInRange = true;
+        }
+        else
+        {
+            isPlayerInRange = false;
+        }
+
+        anim.SetBool("IsAttacking", isPlayerInRange);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {   
+            PlayerHealth playerHealth = collision.GetComponent<PlayerHealth>();
+
+            if (playerHealth != null )
+            {
+                playerHealth.TakeDamage(attackDamage);
+            }
+        }
     }
 }
+
