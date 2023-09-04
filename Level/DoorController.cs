cb54bc3f170d079f141d9d6da265a59b2a284518
Matt Mortensen <mtt.mortensen527@gmail.com>
2023-09-03 18:13:52 -0600

    more bug fixing for door ctrl

--- Level/DoorController.cs
+++ Level/DoorController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
@@ -9,19 +10,46 @@ public class DoorController : MonoBehaviour
     public TileBase[] openDoorTiles;   // Array of tiles for the open door
     private Tilemap tilemap;
 
-    public Vector3Int[] doorTilePositions; // The positions of the door tiles in the Tilemap grid.
+    public List<Vector3Int> doorTilePositions = new List<Vector3Int>(); // The positions of the door tiles in the Tilemap grid.
+
+    public TileBase markerTile;
+    public Tilemap doorTilemap;
 
     private void Start()
     {
         tilemap = GetComponent<Tilemap>();
+
+
+        List<Vector3Int> doorTilePositionsList = new List<Vector3Int>();
+
+        BoundsInt bounds = doorTilemap.cellBounds;
+        TileBase[] allTiles = doorTilemap.GetTilesBlock(bounds);
+
+        for (int x = 0; x < bounds.size.x; x++)
+        {
+            for (int y = 0; y < bounds.size.y; y++)
+            {
+                TileBase tile = allTiles[x + y * bounds.size.x];
+                if (tile == markerTile)
+                {
+                    doorTilePositionsList.Add(new Vector3Int(x, y, 0));
+                    Debug.Log(doorTilePositionsList.Count);
+                }
+            }
+        }
+
+
+        doorTilePositions = doorTilePositions.OrderByDescending(pos => pos.y).ToList();
+
     }
 
     public void OpenDoor()
     {
-        for (int i = 0; i < doorTilePositions.Length; i++)
+        for (int i = 0; i < doorTilePositions.Count; i++)
         {
             tilemap.SetTile(doorTilePositions[i], openDoorTiles[i]);
         }
+
     }
 }
 
