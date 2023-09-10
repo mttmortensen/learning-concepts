6c8cdaef96eb460f5045687a549b5d4ab0f00b05
Matt Mortensen <mtt.mortensen527@gmail.com>
2023-09-10 02:17:55 -0600

    using singleton for manager

--- Enemy/EnemySpawner.cs
+++ Enemy/EnemySpawner.cs
@@ -7,17 +7,18 @@ public class EnemySpawner : MonoBehaviour
     public float spawnInterval;
     public float spacingRadius = 1f; // Radius to check for nearby enemies
     public GameObject enemyPrefab; // Ref to Enemy Prefab (Skelly)
-    public EnemyManager enemyManager; // Reference to the central EnemyManager
     public LayerMask enemyLayerMask; // LayerMask to specify which layers to check for collisions
+    private EnemyManager enemyManager; // Reference to the central EnemyManager
 
     private void Start()
     {
+        enemyManager = EnemyManager.Instance; // Using Singleton
         StartCoroutine(SpawnEnemies());
     }
 
     IEnumerator SpawnEnemies()
     {
-        while (enemyManager.currentNumberOfEnemies < enemyManager.maxNumberOfEnemies)
+        while (!enemyManager.ReachedMaxSpawnList())
         {
             yield return new WaitForSeconds(spawnInterval);
             SpawnEnemy();
@@ -30,7 +31,7 @@ public class EnemySpawner : MonoBehaviour
         Collider2D enemyNearby = Physics2D.OverlapCircle(transform.position, spacingRadius, enemyLayerMask);
 
         // If there's no enemy too close to the spawn point and we haven't reached the max number of enemies
-        if (enemyNearby == null && enemyManager.currentNumberOfEnemies < enemyManager.maxNumberOfEnemies)
+        if (enemyNearby == null && enemyManager.CanSpawnEnemy())
         {
             GameObject spawnedEnemy = Instantiate(enemyPrefab, transform.position, Quaternion.identity);
             EnemyMovement movementScript = spawnedEnemy.GetComponent<EnemyMovement>();
